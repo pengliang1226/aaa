f9b2e901295ef90bf79701501d219add4bc5b431
pengliang1226 <zhaopengliang1226@163.com>
2020-11-23 10:49:02 +0800

    aaa

--- eval_metrics/ScoreStretch.py
+++ eval_metrics/ScoreStretch.py
@@ -5,25 +5,39 @@
 @file: ScoreStretch.py
 @desc: 评分拉伸（预测概率转换为得分）
 """
+from typing import Dict, Union, List, Set
+
 import numpy as np
+import pandas as pd
 from numpy import ndarray
 from pandas import DataFrame
 
 
 class ScoreStretch:
-    def __init__(self, theta: float = None, P0: float = None, PDO: float = None, score_max: float = 1000,
-                 score_min: float = 300, A: float = None, B: float = None, S: float = None, W: float = None):
+    def __init__(self, S: float = None, W: float = None, theta: float = None, P0: float = None, PDO: float = None,
+                 score_max: float = 1000, score_min: float = 300, pred: ndarray = None, extremes: float = 0,
+                 A: float = None, B: float = None):
         """
         初始化函数
+        [注]：计算方式
+        1、通过theta，P0，PDO计算，公式为
+            P0 = A - B * log(theta)
+            P0 - PDO = A - B * log(2*theta)
+        2、通过score_max，score_min，pred，extremes计算，公式为
+            odds = pred / (1 - pred)
+            score_max = A - B * log(odds_min)
+            score_min = A - B * log(odds_max)
+        :param S: 样本坏客率，可根据样本计算
+        :param W: 实际业务数据坏客率，需用户设置
         :param theta: 计算A，B时，需做两个假设，其一odds某个特定违约比率theta下的分数P0; P0 = A - B * log(theta)
         :param P0: 计算A，B时，需做两个假设，其一odds某个特定违约比率theta下的分数P0
         :param PDO: 计算A，B时，需做两个假设，其二该违约比率翻倍时分数的减少值; P0 - PDO = A - B * log(2*theta)
         :param score_max: 分数区间最大值
         :param score_min: 分数区间最小值
+        :param pred: 用于计算A，B训练集预测概率
+        :param extremes: 避免极端值对计算odds_max和odds_min的影响
         :param A: 分数计算公式的常数；score = A - B * log(odds)
         :param B: 分数计算公式的系数
-        :param S: 样本坏客率，可根据样本计算
-        :param W: 实际业务数据坏客率，需用户设置
         """
         # default params
         self.theta = theta
@@ -31,35 +45,43 @@ class ScoreStretch:
         self.PDO = PDO
         self.score_min = score_min
         self.score_max = score_max
+        self.pred = pred
+        self.extremes = extremes
         self.S = S
         assert self.S is not None, '样本坏客率为空'
         self.W = W
-        if theta is not None and P0 is not None and PDO is not None:
-            self.B = PDO / np.log(2)
-            self.A = P0 + self.B * np.log(theta)
-        else:
-            self.A = A
-            self.B = B
+        self.A = A
+        self.B = B
+        self.get_A_B()
 
-    def get_A_B(self, pred: ndarray, extremes: float = 0):
+    def get_A_B(self):
         """
         自适应计算基础分A和翻倍分B
-        :param pred
-        :param extremes: 极值占比，避免结果受极值影响
         :return:
         """
-        odds = np.sort(pred / (1 - pred))
-        odds_max = np.percentile(odds, (1 - extremes) * 100)  # 避免极值干扰
-        odds_min = np.percentile(odds, extremes * 100)  # 避免极值干扰
-        self.B = (self.score_max - self.score_min) / np.log(odds_max / odds_min)
-        if self.W is None:
-            self.A = self.score_min + self.B * np.log(odds_max / (self.S / (1 - self.S)))
+        if self.theta is not None and self.P0 is not None and self.PDO is not None:
+            self.B = self.PDO / np.log(2)
+            self.A = self.P0 + self.B * np.log(self.theta)
+        elif self.score_max is not None and self.score_max is not None and self.pred is not None:
+            if self.W is not None:
+                self.pred = 1 / (1 + ((self.S / (1 - self.S)) / (self.W / (1 - self.W))) * (1 / self.pred - 1))
+            odds = np.sort(self.pred / (1 - self.pred))
+            odds_max = np.percentile(odds, (1 - self.extremes) * 100)  # 避免极值干扰
+            odds_min = np.percentile(odds, self.extremes * 100)  # 避免极值干扰
+            self.B = (self.score_max - self.score_min) / np.log(odds_max / odds_min)
+            if self.W is None:
+                self.A = self.score_min + self.B * np.log(odds_max / (self.S / (1 - self.S)))
+            else:
+                self.A = self.score_min + self.B * np.log(odds_max / (self.W / (1 - self.W)))
+        elif self.A is None or self.B is None:
+            raise Exception('A,B存在缺失值，且无法通过计算得到')
         else:
-            self.A = self.score_min + self.B * np.log(odds_max / (self.W / (1 - self.W)))
+            pass
 
-    def transform(self, pred: ndarray) -> ndarray:
+    def transform_pred_to_score(self, pred: ndarray) -> ndarray:
         """
         预测概率根据实际业务进行还权，并转换为评分
+        :param pred: 预测概率
         :return:
         """
         # 预测概率还权
@@ -69,12 +91,6 @@ class ScoreStretch:
         pred[pred == 1] = 0.9999
         pred[pred == 0] = 0.0001
 
-        # 计算基础分翻倍分
-        if self.A is None and self.B is None:
-            self.get_A_B(pred)
-
-        if self.A is None or self.B is None:
-            raise Exception('基础分, 翻倍分存在空值')
         # 概率转换为得分
         if self.W is None:
             score = self.A - self.B * np.log((pred / (1 - pred)) / (self.S / (1 - self.S)))
@@ -90,4 +106,62 @@ class ScoreStretch:
 
         return score
 
-    # def get_scorecard(self, estimator, bins_info):
+    def transform_data_to_score(self, df: DataFrame, estimator) -> ndarray:
+        """
+        预测概率根据实际业务进行还权，并转换为评分
+        :param df: 数据, 变量顺序需与入模顺序一致, 已进行woe转换
+        :param estimator: 模型
+        :return:
+        """
+        coef = estimator.coef_[0]
+        intercept = estimator.intercept_[0]
+        base_score = self.A - self.B * intercept
+
+        for i, col in enumerate(df.columns):
+            df[col] = df[col].apply(lambda x: -self.B * coef[i] * x)
+
+        score = np.sum(df.values, axis=1)
+        score = np.around(score + base_score, 2)
+
+        return score
+
+    def get_scorecard(self, model_feats: Union[List, Set], estimator, bins_info: Dict) -> DataFrame:
+        """
+        获取评分卡表格
+        :param model_feats: 入模变量
+        :param estimator: 模型
+        :param bins_info: 包含变量属性类型，缺失值单独分箱标志，分箱区间，对应woe值
+        :return:
+        """
+        coef = dict(zip(model_feats, estimator.coef_[0]))
+        intercept = estimator.intercept_[0]
+        base_score = self.A - self.B * intercept
+
+        res = {
+            'name': ['base_score'],
+            'attr_type': [np.nan],
+            'null_flag': [np.nan],
+            'bin': [None],
+            'WOE': [np.nan],
+            'score': [base_score]
+        }
+        for i, k in enumerate(bins_info):
+            feat_type = bins_info[k]['type']
+            feat_flag = bins_info[k]['flag']
+            feat_bins = bins_info[k]['bins']
+            feat_woes = bins_info[k]['woes']
+
+            for j, v in enumerate(feat_bins):
+                res['name'].append(k)
+                res['attr_type'].append(feat_type)
+                if feat_flag == 1 and j == 0:
+                    res['null_flag'].append(1)
+                else:
+                    res['null_flag'].append(0)
+                res['bin'].append(feat_bins[j])
+                res['WOE'].append(feat_woes[j])
+                res['score'].append(round(-self.B * feat_woes[j] * coef[k], 4))
+
+        res = pd.DataFrame(res)
+
+        return res
--- scorecard.py
+++ scorecard.py
@@ -2,7 +2,7 @@
 """
 @author: pengliang.zhao
 @time: 2020/10/21 19:34
-@file: test.py
+@file: scorecard.py
 @desc: 
 """
 import re
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split
 
 from eval_metrics import ScoreStretch, calc_ks, calc_auc
 from feature_binning import DecisionTreeBinner, QuantileBinner
+from model_training import BasicTrainer
 from feature_select import *
 from util import *
 
@@ -51,7 +52,7 @@ if __name__ == '__main__':
 
     # ----------------------------------------------PSI筛选----------------------------------------------#
     QT = QuantileBinner(features_info=features_type, features_nan_value=null_flag, max_leaf_nodes=6)
-    QT.fit(train_data.loc[:, first_feats], train_data['y'])
+    QT.fit(train_data, train_data['y'], first_feats)
 
     res = PSI_filter(train_data[train_data['user_date'] < '2018-04-30'],
                      train_data[train_data['user_date'] >= '2018-04-30'], bins_info=QT.features_bins,
@@ -61,56 +62,49 @@ if __name__ == '__main__':
 
     # ----------------------------------------------iv值筛选--------------------------------------------------#
     DT = DecisionTreeBinner(features_info=features_type, features_nan_value=null_flag, max_leaf_nodes=6)
-    DT.fit(train_data.loc[:, second_feats], train_data['y'])
+    DT.fit(train_data, train_data['y'], second_feats)
 
-    special = []
-    # 剔除分箱区间数为1的变量
-    for k, v in DT.features_bins.items():
-        if (v['flag'] == 1 and len(v['bins']) == 2) or (v['flag'] == 0 and len(v['bins']) == 1):
-            special.append(k)
+    # 对IV值大于0.02变量调整分箱单调性
+    tmp = [k for k, v in DT.features_iv.items() if v > 0.02]
+    DT.binning_trim(train_data, train_data['y'], tmp)
+    # 分箱区间数大于1且iv值大于0.02的变量
+    tmp = {}
+    for col in second_feats:
+        iv = DT.features_iv[col]
+        flag = DT.features_bins[col]['flag']
+        bins_len = len(DT.features_bins[col]['bins'])
+        if iv <= 0.02:
+            continue
+        if (flag == 1 and bins_len == 2) or (flag == 0 and bins_len == 1):
+            continue
+        tmp[col] = iv
 
-    # 剔除IV值小于0.02变量
-    for k, v in DT.features_iv.items():
-        if v <= 0.02:
-            special.append(k)
-
-    third_feats = [v[0] for v in sorted(DT.features_iv.items(), key=lambda x: x[1], reverse=True) if
-                   v[0] not in special]
-
-    # ----------------------------------------------相关系数筛选----------------------------------------------#
-    forth_data = train_data.copy()
-    for col in third_feats:
-        if forth_data[col].dtype == 'O':
-            col_data = forth_data[col]
-            tmp = disorder_mapping(col_data, forth_data['y'], 1, null_flag[col])
-            forth_data[col] = col_data.map(tmp).fillna(-1)
-    forth_feats = correlation_filter(forth_data, third_feats)
-
-    # ----------------------------------------------多重共线性筛选----------------------------------------------#
-    fifth_feats = vif_filter(forth_data, forth_feats, threshold=10)
-
-    # ----------------------------------------------调整分箱并重新筛选iv----------------------------------------------#
-    DT.binning_trim(train_data.loc[:, fifth_feats], train_data['y'], fifth_feats)
-    six_feats = [k for k in fifth_feats if DT.features_iv[k] > 0.02]
+    third_feats = [v[0] for v in sorted(tmp.items(), key=lambda x: x[1], reverse=True)]
 
     # -----------------------------------------------woe转码---------------------------------------------------#
-    train_data = train_data.loc[:, ['y', 'user_date'] + six_feats]
-    test_data = test_data.loc[:, ['y', 'user_date'] + six_feats]
-    for col in six_feats:
+    train_data = train_data.loc[:, ['y', 'user_date'] + third_feats]
+    test_data = test_data.loc[:, ['y', 'user_date'] + third_feats]
+    for col in third_feats:
         train_data[col] = woe_transform(train_data[col], features_type[col], DT.features_bins[col],
                                         DT.features_woes[col])
         test_data[col] = woe_transform(test_data[col], features_type[col], DT.features_bins[col], DT.features_woes[col])
 
+    # ----------------------------------------------相关系数筛选----------------------------------------------#
+    forth_feats = correlation_filter(train_data, third_feats)
+
+    # ----------------------------------------------多重共线性筛选----------------------------------------------#
+    fifth_feats = vif_filter(train_data, forth_feats, threshold=10)
+
     # ---------------------------------------------------剔除系数为负数的特征-----------------------------------------#
-    seven_feats = coef_forward_filter(train_data, train_data['y'], six_feats)
+    six_feats = coef_forward_filter(train_data, train_data['y'], fifth_feats)
 
     # ------------------------------------------------显著性筛选------------------------------------------------------#
-    eight_feats = logit_pvalue_forward_filter(train_data, train_data['y'], seven_feats)
+    seven_feats = logit_pvalue_forward_filter(train_data, train_data['y'], six_feats)
 
     # -----------------------------------------训练模型------------------------------------------#
-    train_x, val_x, train_y, val_y = train_test_split(train_data[eight_feats], train_data['y'], test_size=0.2,
+    train_x, val_x, train_y, val_y = train_test_split(train_data[seven_feats], train_data['y'], test_size=0.2,
                                                       random_state=123)
-    test_x, test_y = test_data[eight_feats], test_data['y']
+    test_x, test_y = test_data[seven_feats], test_data['y']
     params = {
         # 默认参数
         "solver": 'liblinear',
@@ -121,19 +115,15 @@ if __name__ == '__main__':
         "C": 1.0,
         "random_state": 0
     }
-    lr_model = LogisticRegression(**params).fit(train_x, train_y)
+    model = BasicTrainer(algorithm='lr', params=params)
+    model.fit(train_x, train_y)
+    val_pred = model.estimator.predict_proba(val_x)[:, -1]
+    test_pred = model.estimator.predict_proba(test_x)[:, -1]
 
     sc = ScoreStretch(S=ori_data['y'].sum() / ori_data.shape[0])
-    train_pred = sc.predict(train_x, lr_model)
-    train_score = sc.predict_transform(train_x, lr_model)
-    val_pred = sc.predict(val_x, lr_model)
-    val_score = sc.predict_transform(val_x, lr_model)
-    test_pred = sc.predict(test_x, lr_model)
-    test_score = sc.predict_transform(test_x, lr_model)
 
     val_ks = calc_ks(val_y, val_pred)
     test_ks = calc_ks(test_y, test_pred)
-
     val_auc = calc_auc(val_y, val_pred)
     test_auc = calc_auc(test_y, test_pred)
     print('验证集auc={}, ks={}'.format(val_auc, val_ks))
