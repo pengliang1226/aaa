44e7461dd7fb9e62f8fba17757ac7fe033bdda31
pengliang1226 <zhaopengliang1226@163.com>
2020-12-04 14:05:56 +0800

    aaa

---
+++ feature_extraction/__init__.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+"""
+@author: pengliang.zhao
+@time: 2020/11/30 16:00
+@file: __init__.py.py
+@desc: 
+"""
+from feature_extraction.DimensionReduction import DimensionReduction
+
+
+__all__ = [
+    'DimensionReduction',  # 降维
+]
