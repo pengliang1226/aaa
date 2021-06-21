185d5cd3072ff3636ac69c5324cb42b094309a70
pengliang1226 <zhaopengliang1226@163.com>
2021-06-21 19:04:58 +0800

    aa

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ DESCRIPTION = 'all model func'
 AUTHOR = 'pengliang.zhao'
 REQUIRES_PYTHON = '>=3.7'
 VERSION = '0.0.1'
-REQUIRED = ['numpy==1.18.1', 'pandas==1.0.5', 'scikit-learn==0.23.1', 'xgboost==1.1.1', 'lightgbm==2.3.1',
-            'scipy==1.4.1', 'statsmodels==0.10.1', 'mlxtend==0.17.3', 'nni==1.9', 'category-encoders==2.2.2']
+REQUIRED = ['numpy', 'pandas', 'scikit-learn', 'lightgbm',
+            'scipy', 'statsmodels', 'mlxtend', 'category-encoders']
 
 setup(
     name=NAME,
